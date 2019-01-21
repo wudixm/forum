c9d95007b0850b81e4603a1f4eb5273256e474ee
wudixm <wxm66668888@163.com>
2019-01-21 09:48:58 +0800

    add http kit for run-server function

--- project.clj
+++ project.clj
@@ -3,4 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [compojure "1.1.8"]
+                 [http-kit "2.1.16"]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [mysql/mysql-connector-java "8.0.11"]
+                 [hiccup "1.0.5"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-core "1.6.3"]
+                 [org.clojure/data.json "0.2.6"]
+                 ]
+  )
--- src/forum/core.clj
+++ src/forum/core.clj
@@ -1,6 +1,21 @@
-(ns forum.core)
+(ns forum.core
+  (require
+    [org.httpkit.server :refer [run-server]]
+    )
+  )
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+
+
+(defn -main []
+  (println "start server at port 8080")
+  (run-server handler {:port 8080}))
