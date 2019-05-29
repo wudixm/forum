59dfac05b406d2989190ff15167d32ee5199b65b
wudixm <wxm66668888@163.com>
2019-05-29 10:22:43 +0800

    change db-spec according machine

--- src/forum/common/util/db.clj
+++ src/forum/common/util/db.clj
@@ -1,9 +1,19 @@
 (ns forum.common.util.db
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.data.json :as json]
-            ))
+            )
+  (:import (java.net InetAddress)))
 
-(def db-spec
+(def local_mac
+  {:classname   "com.mysql.jdbc.Driver"
+   :subprotocol "mysql"
+   :subname           "//127.0.0.1:13308/forum"
+   ; :subname           "//172.17.0.1:3306/forum"
+   :user        "dev"
+   :password    "devdev"
+   :useSSL false
+   })
+(def remote_ali
   {:classname   "com.mysql.jdbc.Driver"
    :subprotocol "mysql"
    ; :subname           "//127.0.0.1:13308/forum"
@@ -14,3 +24,21 @@
    :password    "devdevdev"
    :useSSL false
    })
+(defn is_local_mac []
+  (let [host_str (.toString (InetAddress/getLocalHost))]
+    (println host_str)
+    (println "host_str")
+    (if (> (.indexOf host_str "hoytMacBook") -1)
+
+      true
+      false
+      )
+    )
+  )
+(def db-spec
+  (if (is_local_mac)
+    local_mac
+    remote_ali
+    )
+  )
+(println db-spec)
